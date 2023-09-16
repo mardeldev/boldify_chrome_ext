c3ec8aa48beea13635e2a1d9528c250cda5383d7
Aimable Mardel <73969781+aimardel@users.noreply.github.com>
2023-09-16 21:05:22 +0100

    Added font options

--- content.js
+++ content.js
@@ -1,21 +1,114 @@
 
-function makeFirstTwoLettersBold(element) {
+try {
+    chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+        if (request.action === "toggleBold") {
+            propertyOption = request.property;
+            toggleBoldEffect(propertyOption);
+        }
+    });
+
+} catch (error) {
+    console.log(error);
+}
+
+var isBoldActive = false;
+
+
+function toggleBoldEffect(propertyOption) {
+
+    var paragraphs = document.querySelectorAll('p, h1, h2, h3, h4, h5, h6, div, li, #text, span');
+
+    for (var i = 0; i < paragraphs.length; i++) {
+        if (isBoldActive) {
+            revertBoldEffect(paragraphs[i]);
+        } else {
+            makeFirstTwoLettersBold(paragraphs[i], propertyOption);
+
+        }
+    }
+    isBoldActive = !isBoldActive;
+}
+
+
+
+let replacedChar = '';
+
+function checkQuotations(words) {
+    let openDoubleQuote = 8220;
+    let openSingleQuote = 8216;
+    let openSquareBracket = 91;
+    let openCurlyBracket = 123;
+    let openParenthesis = 40;
+
+    switch (words.codePointAt(0)) {
+        case (openDoubleQuote):
+            replacedChar = openDoubleQuote;
+            return true;
+        case (openSingleQuote):
+            replacedChar = openSingleQuote;
+            return true;
+        case (openSquareBracket):
+            replacedChar = openSquareBracket;
+            return true;
+        case (openCurlyBracket):
+            replacedChar = openCurlyBracket;
+            return true;
+        case (openParenthesis):
+            replacedChar = openParenthesis;
+            return true;
+        default:
+            break;
+    }
+}
+
+
+function makeFirstTwoLettersBold(element, propertyOption) {
     let node, walk = document.createTreeWalker(element, NodeFilter.SHOW_TEXT, null, false);
+    element.originalElement = element.cloneNode(true);
+
 
     while (node = walk.nextNode()) {
+
+
+
         let words = node.textContent.split(' ');
-        for (let i = 0; i < words.length; i++) {
-            words[i] = words[i].replace(/\b\w{1,2}/, function (match) {
-                return `<strong>${match}</strong>`;
-            });
-        }
         let newNode = document.createElement('span');
-        newNode.innerHTML = words.join(' ');
+
+
+        for (let i = 0; i < words.length; i++) {
+            if (words[i].length > 0) {
+                let start = 0;
+                let end = 2;
+
+
+                let spanElement = document.createElement('span');
+                spanElement.className = propertyOption;
+
+                spanElement.textContent = checkQuotations(words[i]) == true ? `${String.fromCodePoint(replacedChar)}${words[i].substring(start += 1, end += 1)}` : `${words[i].substring(start, end)}`
+
+
+                newNode.appendChild(spanElement);
+                newNode.appendChild(document.createTextNode(words[i].substring(end)));
+
+
+                if (i < words.length - 1) {
+                    newNode.appendChild(document.createTextNode(' '));
+                }
+            }
+        }
         node.parentNode.replaceChild(newNode, node);
     }
 }
 
-var paragraphs = document.querySelectorAll('p:not(span), h1:not(span), h2:not(span), h3:not(span), h4:not(span), h5:not(span), h5:not(span), div:not(span), li:not(span)');
-for (var i = 0; i < paragraphs.length; i++) {
-    makeFirstTwoLettersBold(paragraphs[i]);
+function revertBoldEffect(element) {
+
+    if (element.originalElement) {
+        element.parentNode.replaceChild(element.originalElement, element);
+    }
 }
+
+
+
+
+
+
--- popup.js
+++ popup.js
@@ -1,21 +1,33 @@
 
+let fontOption = document.getElementById('boldifyFonts');
 
-document.addEventListener('DOMContentLoaded', function () {
-    var toggleMagnifyButton = document.getElementById('toggleMagnify');
-    toggleMagnifyButton.addEventListener('click', function () {
-        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, { action: "toggleMagnify" });
-        });
-    });
+let propertyOption = 'boldLetters';
 
-});
+fontOption.addEventListener('input', function () {
+    switch (fontOption.value) {
+        case 'Courier New':
+            propertyOption = 'boldLettersCourierNew';
+            break;
+        case 'Georgia':
+            propertyOption = 'boldLettersGeorgia';
+            break;
+        case 'Arial':
+            propertyOption = 'boldLettersArial';
+        default:
+            propertyOption = 'boldLetters';
+            break;
+    }
+
+})
+
+// let propertyOption = 'boldLetters';
+
 
 document.addEventListener('DOMContentLoaded', function () {
     var makeBoldButton = document.getElementById('makeBold');
     makeBoldButton.addEventListener('click', function () {
-        chrome.tabs.executeScript({
-            file: 'content.js'
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, { action: 'toggleBold', property: propertyOption });
         });
     });
 });
-
